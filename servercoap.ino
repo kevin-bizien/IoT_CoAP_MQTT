41c397dfc317309d002e6727d86a32c88d470ee4
kevin-bizien <40176936+kevin-bizien@users.noreply.github.com>
2018-06-13 11:54:09 +0200

    final

---
+++ servercoap.ino
@@ -0,0 +1,96 @@
+/*
+* WARNING - UDP_TX_PACKET_MAX_SIZE is hardcoded by Arduino to 24 bytes
+* This limits the size of possible outbound UDP packets
+* https://internetofhomethings.com/homethings/?tag=esp8266-coap
+*/
+
+#include <SPI.h>
+#include <Ethernet2.h>
+#include <stdint.h>
+#include <EthernetUdp2.h>
+#include "coap.h"
+
+
+#define PORT 5683
+static uint8_t mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x05, 0x0F};
+
+IPAddress ip(192, 168, 1, 177);
+EthernetServer server(80);
+EthernetClient client;
+EthernetUDP udp;
+uint8_t packetbuf[256];
+static uint8_t scratch_raw[32];
+static coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
+
+void setup()
+{
+    int i;
+    Serial.begin(9600);
+    while (!Serial) 
+    {
+        ; // wait for serial port to connect. Needed for Leonardo only
+    }
+
+    // start the Ethernet connection:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+    Serial.println();
+    udp.begin(PORT);
+
+    coap_setup();
+    endpoint_setup();
+}
+
+void udp_send(const uint8_t *buf, int buflen)
+{
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    while(buflen--)
+        udp.write(*buf++);
+    udp.endPacket();
+}
+
+void loop()
+{
+    int sz;
+    int rc;
+    coap_packet_t pkt;
+    int i;
+    
+    if ((sz = udp.parsePacket()) > 0)
+    {
+        udp.read(packetbuf, sizeof(packetbuf));
+
+        for (i=0;i<sz;i++)
+        {
+            Serial.print(packetbuf[i], HEX);
+            Serial.print(" ");
+        }
+        Serial.println("");
+
+        if (0 != (rc = coap_parse(&pkt, packetbuf, sz)))
+        {
+            Serial.print("Bad packet rc=");
+            Serial.println(rc, DEC);
+        }
+        else
+        {
+            size_t rsplen = sizeof(packetbuf);
+            coap_packet_t rsppkt;
+            coap_handle_req(&scratch_buf, &pkt, &rsppkt);
+
+            memset(packetbuf, 0, UDP_TX_PACKET_MAX_SIZE);
+            if (0 != (rc = coap_build(packetbuf, &rsplen, &rsppkt)))
+            {
+                Serial.print("coap_build failed rc=");
+                Serial.println(rc, DEC);
+            }
+            else
+            {
+                udp_send(packetbuf, rsplen);
+            }
+        }
+    }
+}
+
